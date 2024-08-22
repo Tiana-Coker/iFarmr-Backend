a48069f8087ad02252afc046f2ea8268d94dedeb
abayomiogunlowo <abayomiogunlowo@gmail.com>
2024-08-22 09:57:33 +0100

    Resolved Conflicts

--- src/main/java/org/ifarmr/controller/UserController.java
+++ src/main/java/org/ifarmr/controller/UserController.java
@@ -3,9 +3,11 @@ package org.ifarmr.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.ifarmr.payload.request.UserDetailsDto;
 import org.ifarmr.payload.response.CloudinaryResponse;
 import org.ifarmr.repository.UserRepository;
 import org.ifarmr.service.FileUploadService;
+import org.ifarmr.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,9 +17,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/user")
 public class UserController {
 
+    private final UserService userService;
     private final FileUploadService fileUploadService;
 
     @PutMapping("/upload/profilephoto")
@@ -25,6 +28,14 @@ public class UserController {
         String username = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
 
         return ResponseEntity.ok(fileUploadService.uploadProfilePicture (username, file));
+    }
+
+    // Edit User Details
+    @PutMapping("/edit-user-details")
+    public ResponseEntity<UserDetailsDto> editUserDetails(@RequestBody UserDetailsDto adminUserDetailsDto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        return ResponseEntity.ok(userService.editUserDetails(currentUsername, adminUserDetailsDto));
     }
 
 }
