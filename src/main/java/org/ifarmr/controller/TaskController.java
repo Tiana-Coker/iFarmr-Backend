ef814ec11c02dc6bcfc6e93664af19bce5140503
Tiana Coker <tianacoker1@gmail.com>
2024-08-27 14:10:43 +0100

    feat: add endpoint to get list of upcoming tasks in order of their due dates

--- src/main/java/org/ifarmr/controller/TaskController.java
+++ src/main/java/org/ifarmr/controller/TaskController.java
@@ -5,16 +5,16 @@ import org.ifarmr.entity.User;
 import org.ifarmr.exceptions.NotFoundException;
 import org.ifarmr.payload.request.NewTaskRequest;
 import org.ifarmr.payload.response.TaskResponseDto;
+import org.ifarmr.payload.response.UpcomingTaskResponse;
 import org.ifarmr.service.TaskService;
 import org.ifarmr.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
@@ -34,5 +34,15 @@ public class TaskController {
         TaskResponseDto createdTask = taskService.createTask(taskRequest, currentUsername);
 
         return ResponseEntity.status(201).body(createdTask);
+    }
+
+    @GetMapping("/upcoming")
+    public ResponseEntity<List<UpcomingTaskResponse>> getUpcomingTasks(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+
+        List<UpcomingTaskResponse> upcomingTasks = taskService.getUpcomingTasks(currentUsername);
+        return ResponseEntity.ok(upcomingTasks);
+
     }
 }
--- src/main/java/org/ifarmr/service/TaskService.java
+++ src/main/java/org/ifarmr/service/TaskService.java
@@ -4,7 +4,11 @@ import org.ifarmr.entity.Task;
 import org.ifarmr.entity.User;
 import org.ifarmr.payload.request.NewTaskRequest;
 import org.ifarmr.payload.response.TaskResponseDto;
+import org.ifarmr.payload.response.UpcomingTaskResponse;
+
+import java.util.List;
 
 public interface TaskService {
     TaskResponseDto createTask(NewTaskRequest taskRequest, String currentUsername);
+    List<UpcomingTaskResponse> getUpcomingTasks(String username);
 }
