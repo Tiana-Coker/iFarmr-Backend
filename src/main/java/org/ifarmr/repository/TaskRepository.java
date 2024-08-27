0e8352c3f662a7edc50b766b93c10bb398eaebda
abayomiogunlowo <abayomiogunlowo@gmail.com>
2024-08-28 00:32:35 +0100

    Merge branch 'develop' into Endpoint-to-comment-&-like-on-posts

--- src/main/java/org/ifarmr/repository/TaskRepository.java
+++ src/main/java/org/ifarmr/repository/TaskRepository.java
@@ -3,9 +3,15 @@ package org.ifarmr.repository;
 import org.ifarmr.entity.Task;
 import org.ifarmr.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     boolean existsByTitleAndDueDateAndUser(String title, LocalDate dueDate, User user);
+
+    @Query("SELECT t FROM Task t WHERE t.user = :user ORDER BY t.dueDate ASC")
+    List<Task> findTasksByUserOrderByDueDateAsc(@Param("user") User user);
 }
