829ebb32726cc6cd15ee0c81976a2fd341cbe4cf
Chinecherem <chinecheremubawike@gmail.com>
2024-08-29 08:21:10 +0100

    Updated swagger DOCS to enable testing multipart files on swagger UI

--- src/main/java/org/ifarmr/config/SpringDocConfig.java
+++ src/main/java/org/ifarmr/config/SpringDocConfig.java
@@ -8,6 +8,12 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+
+
 @Configuration
 public class SpringDocConfig {
 
@@ -27,7 +33,19 @@ public class SpringDocConfig {
                                 .bearerFormat("JWT")
                                 .in(SecurityScheme.In.HEADER)
                                 .name("Authorization")
-                        ))
+                        )
+
+                        // Define a request body for multipart form data
+                        .addRequestBodies("multipartRequestBody",
+                                new RequestBody().content(new Content().addMediaType(
+                                        org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE,
+                                        new MediaType().schema(new Schema<>().type("object")
+                                                .addProperties("title", new Schema<>().type("string"))
+                                                .addProperties("content", new Schema<>().type("string"))
+                                                .addProperties("photo", new Schema<>().type("string").format("binary"))
+                                        ))))
+                )
+
                 // Global security requirement
                 .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
     }
--- src/main/java/org/ifarmr/controller/PostController.java
+++ src/main/java/org/ifarmr/controller/PostController.java
@@ -1,12 +1,16 @@
 package org.ifarmr.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.ifarmr.entity.Post;
 import org.ifarmr.payload.request.PostRequest;
 import org.ifarmr.payload.response.PopularPostResponse;
 import org.ifarmr.payload.response.PostResponse;
 import org.ifarmr.service.PostService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,7 +25,7 @@ public class PostController {
 
     private final PostService postService;
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<PostResponse> createPost(@ModelAttribute PostRequest postRequest) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
