a90e771edef00c67348df5312dc8e981388b1260
Chinecherem <chinecheremubawike@gmail.com>
2024-10-04 18:34:03 +0100

    Functionality to enable Roles Dataloader run before Admin initializer

--- src/main/java/org/ifarmr/utils/DataLoader.java
+++ src/main/java/org/ifarmr/utils/DataLoader.java
@@ -6,9 +6,11 @@ import org.ifarmr.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(1)  // Ensures that DataLoader runs before AdminInitializer
 public class DataLoader implements ApplicationRunner {
 
     @Autowired
